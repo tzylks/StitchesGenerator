20f2e2b1fcd30136c70046f2a3d9c7ae39deacee
TrevorZylks <trevorzylks@gmail.com>
2023-05-05 15:22:42 -0700

    type error

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -18,7 +18,7 @@ const ApiDashboard = async ({}) => {
         where: { userId: user.user.id },
     });
 
-    const activeApiKey = apiKeys.find((key) => key.enabled);
+    const activeApiKey = apiKeys.find((key: any) => key.enabled);
 
     if (!activeApiKey) return notFound();
 
